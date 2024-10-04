eda57828d05aaebfc193dcc8dfe6df418e5fc97f
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-10-04 18:29:48 -0300

    Setup Express server and added /api/auth route for user authentication

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("server is running");
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+
+app.use("/api/auth", authRoutes);
+
+app.listen(8000, () => {
+  console.log("Server is running on port 8000");
+});
