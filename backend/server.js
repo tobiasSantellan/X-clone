a80f75b716d9232b759ba122afe4c95bc1de164c
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-10-05 21:53:49 -0300

    Configured environment variables, connected MongoDB, and set up authentication routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectMongoDB from "./db/connectMongoDB.js";
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+console.log(process.env.MONGO_URI);
 
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+  connectMongoDB();
 });
