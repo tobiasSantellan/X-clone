508663b8f32f63a5d25cf0414ed40c2f3ab3b8a8
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-10-11 22:01:13 -0300

    Add cookie-parser middleware to handle cookies in the application

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse req.body
 
 app.use("/api/auth", authRoutes);
+
+app.use(cookieParser());
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
