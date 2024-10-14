9ad6a9e824aa2e45eda87a4e7b72de94df88ed6d
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-10-14 15:14:09 -0300

    add route for post-related endpoints in API

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import userRoutes from "./routes/user.routes.js";
+import postRoutes from ".routes/post.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
@@ -21,6 +22,7 @@ app.use(express.json()); // to parse req.body
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.use(cookieParser());
 
