20459026fee2418f5ead13c72ac2313589633e46
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-10-16 22:33:28 -0300

    Extend post routes and add controllers

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import { protectRoute } from "../middleware/protectRoute";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { createPost } from "../controllers/post.controllers.js";
 
 const router = express.Router();
 
-router.post("/create", protectRoute);
+router.post("/create", protectRoute, createPost);
+router.post("/like:id", protectRoute, linkeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
+router.delete("/", protectRoute, deletePost);
 
 export default router;
