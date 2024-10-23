6db6656e0098cd9278a12aad39a3770d43dd0183
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-10-23 17:47:26 -0300

    add post-related routes with protection middleware

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,11 +1,24 @@
 import express from "express";
 import { protectRoute } from "../middleware/protectRoute.js";
-import { createPost } from "../controllers/post.controllers.js";
+import {
+  createPost,
+  commentOnPost,
+  deletePost,
+  likeUnlikePost,
+  getAllPosts,
+  getLikedPosts,
+  getFollowingPosts,
+  getUserPosts,
+} from "../controllers/post.controllers.js";
 
 const router = express.Router();
 
+router.get("/all", protectRoute, getAllPosts);
+router.get("/following", protectRoute, getFollowingPosts);
+router.get("/likes/:id", protectRoute, getLikedPosts);
+router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
-router.post("/like:id", protectRoute, linkeUnlikePost);
+router.post("/like:id", protectRoute, likeUnlikePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/", protectRoute, deletePost);
 
