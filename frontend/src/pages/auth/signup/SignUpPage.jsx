850eaf9e75a726e1a994c9e7dfec08b49622f1f5
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-10-28 00:18:20 -0300

    Add signup mutation function with error handling and toast notification

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -7,6 +7,8 @@ import { MdOutlineMail } from "react-icons/md";
 import { FaUser } from "react-icons/fa";
 import { MdPassword } from "react-icons/md";
 import { MdDriveFileRenameOutline } from "react-icons/md";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const SignUpPage = () => {
   const [formData, setFormData] = useState({
@@ -14,6 +16,27 @@ const SignUpPage = () => {
     username: "",
     fullName: "",
     password: "",
+  });
+
+  const { mutate, isError, isPending, error } = useMutation({
+    mutationFn: async (email, username, fullName, password) => {
+      try {
+        const res = await fetch("api/auth/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, username, fullName, password }),
+        });
+        if (!res.ok) throw new Error("Something went wrong");
+        const data = await res.json();
+        if (data.error) throw new Error(data.error);
+        console.log("Data:", data);
+        return data;
+      } catch (error) {
+        toast.error(error.message);
+      }
+    },
   });
 
   const handleSubmit = (e) => {
@@ -25,7 +48,7 @@ const SignUpPage = () => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const isError = false;
+  // const isError = false;
 
   return (
     <div className="max-w-screen-xl mx-auto flex h-screen px-10">
