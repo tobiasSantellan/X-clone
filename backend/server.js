6078e57d857ab744f04958a22586a2ff68c037ca
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-10-29 00:16:15 -0300

    add express.urlencoded middleware to parse URL-encoded request bodies

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
