5c633e1e20ae6660bbbd0f2c4ce03d9f12a44328
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-10-30 19:40:36 -0300

    feat: Add console logs in protectRoute middleware for better debugging and error tracking

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -2,18 +2,23 @@ import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
+  console.log("Middleware protectRoute triggered");
+  console.log("Cookies received:", req.cookies);
   try {
     const token = req.cookies.jwt;
+    console.error("Token:", token);
     if (!token) {
       return res.status(401).json({ error: "Unauthorized: No Token Provided" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded:", decoded);
     if (!decoded) {
       return res.status(401).json({ error: "Unauthorized: Invalid Token" });
     }
 
     const user = await User.findById(decoded.userId).select("-password");
+    console.error("User:", user);
 
     if (!user) {
       return res.status(404).json({ error: "User not found" });
@@ -22,6 +27,7 @@ export const protectRoute = async (req, res, next) => {
     req.user = user;
     next();
   } catch (error) {
+    console.error("Error in protectRoute:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
