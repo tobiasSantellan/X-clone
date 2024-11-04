ec8f15e4376bd19d86425140ca65df78c927ceb0
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-11-04 07:53:31 -0300

    add a fix in error handling

--- frontend/src/components/common/RightPanel.jsx
+++ frontend/src/components/common/RightPanel.jsx
@@ -11,7 +11,7 @@ const RightPanel = () => {
       try {
         const res = await fetch("/api/users/suggested");
         const data = await res.json();
-        if (!res.ok) throw new Error(data.message || "Somethin went wrong");
+        if (!res.ok) throw new Error(data.error || "Somethin went wrong");
         return data;
       } catch (error) {
         throw new Error(error.message);
