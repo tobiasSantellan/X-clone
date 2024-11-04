f3fabb7ba38bc85aa85201bfe16b0adecb7d4c8c
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-11-04 09:25:13 -0300

    debug

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -162,15 +162,11 @@ const ProfilePage = () => {
                 {(coverImg || profileImg) && (
                   <button
                     className="btn btn-primary rounded-full btn-sm text-white px-4 ml-2"
-                    onClick={
-                      (async () =>
-                        await updateProfile({
-                          coverImg,
-                          profileImg,
-                        }),
-                      setCoverImg(null),
-                      setProfileImg(null))
-                    }
+                    onClick={async () => {
+                      await updateProfile({ coverImg, profileImg });
+                      setProfileImg(null);
+                      setCoverImg(null);
+                    }}
                   >
                     {isUpdatingProfile ? "Updating..." : "Update"}
                   </button>
