f7620dca65408395432aa5011580f53555782c1e
Tobias Santellan <tobias.ariel.santellan@gmail.com>
2024-11-04 07:50:46 -0300

    removed console

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -2,17 +2,15 @@ import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
 
 export const protectRoute = async (req, res, next) => {
-  console.log("Middleware protectRoute triggered");
-  console.log("Cookies received:", req.cookies);
   try {
     const token = req.cookies.jwt;
-    console.error("Token:", token);
+
     if (!token) {
       return res.status(401).json({ error: "Unauthorized: No Token Provided" });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Decoded:", decoded);
+
     if (!decoded) {
       return res.status(401).json({ error: "Unauthorized: Invalid Token" });
     }
